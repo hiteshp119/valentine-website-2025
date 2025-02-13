ecbf6b90bf167f149e88bf431e896058536027e9
hiteshp119 <168054759+hiteshp119@users.noreply.github.com>
2025-02-13 15:43:21 -0600

    Update config.js

--- config.js
+++ config.js
@@ -51,8 +51,8 @@ const CONFIG = {
     celebration: {
         title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
         message: "Now come get your gift, a big warm hug and a huge kiss!",
-        emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
-        imageUrl: "https://raw.github.com/hiteshp119/valentine-website-2025/main/IMG_6197-ezgif.com-video-to-gif-converter.gif"
+        emojis: "🎁💖🤗💝💋❤️💕",  // These will bounce around
+        imageUrl: "https://raw.githubusercontent.com/hiteshp119/valentine-website-2025/main/IMG_6197-ezgif.com-video-to-gif-converter.gif"
     },
 
     // Color scheme for the website
